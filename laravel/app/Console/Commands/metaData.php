3d035263cdd7f570f4e2d3f0e49e3fd7298d598f
Mahmoud Trro <mahdevtrro@two.sa>
2023-11-17 22:06:41 +0300

    commands for other platforms

---
+++ laravel/app/Console/Commands/metaData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class metaData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:meta-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
