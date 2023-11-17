aada038f658eed7a9e2672335bb9d67a292bc814
Mahmoud Trro <mahdevtrro@two.sa>
2023-11-17 21:58:38 +0300

    add command for cron job

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -13,6 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('app:tiktok-data')->daily();
+        $schedule->command('app:snapchat-data')->daily();
+        $schedule->command('app:meta-data')->daily();
     }
 
     /**
