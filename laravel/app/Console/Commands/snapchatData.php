2a200beae7045c14692c458cf972b7fa2b0c1d31
Mahmoud Trro <mahdevtrro@two.sa>
2023-12-02 21:59:56 +0300

    snapchat reports + models + migrations

--- laravel/app/Console/Commands/snapchatData.php
+++ laravel/app/Console/Commands/snapchatData.php
@@ -373,7 +373,7 @@ class snapchatData extends Command
 
             $response = Http::withHeaders([
                 'Content-Type' => 'application/x-www-form-urlencoded',
-            ])->post($apiEndpoint, [
+            ])->asForm()->post($apiEndpoint, [
                 'refresh_token' => $refresh_token,
                 'client_id' => $client_id,
                 'client_secret' => $client_secret,
