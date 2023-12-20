162661916376d5c8c24c0b9c069f3e35a3d87c39
Mahmoud Trro <mahdevtrro@two.sa>
2023-12-20 21:37:50 +0300

    meta models + migrations

--- laravel/app/Console/Commands/metaData.php
+++ laravel/app/Console/Commands/metaData.php
@@ -2,7 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Models\AudienceNetworkCampaignReport;
+use App\Models\AudienceNetwrokAdReport;
 use App\Models\CampaignsReport;
+use App\Models\FacebookAdReport;
+use App\Models\FacebookCampaignReport;
+use App\Models\InstagramAdReport;
+use App\Models\InstagramCampaignReport;
+use App\Models\MessengerAdReport;
+use App\Models\MessengerCampaignReport;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -44,29 +52,43 @@ class metaData extends Command
 //                'Access-Token' => config('services.tiktok.access_token'),
             ])->get($apiEndpoint, [
                 'fields' => json_encode(["impressions","campaign_id","campaign_name","clicks","purchase_roas","spend","conversion_values","conversions"]),
-                'data_level' => 'AUCTION_CAMPAIGN',
-                'dimensions' => json_encode(["campaign_id","stat_time_day"]),
-                'service_type' => 'AUCTION',
-                'report_type' => 'BASIC',
+                'level' => 'campaign',
+                'time_range' => json_encode(["since"=>Carbon::now()->format('Y-m-d'),
+                    "until"=>Carbon::now()->format('Y-m-d')]),
+                'time_increment' => '1',
+                'breakdowns' => 'publisher_platform',
                 'page_size' => 50,
-                'page'  =>  1,
-                'start_date' => Carbon::now()->format('Y-m-d'),
-                'end_date' => Carbon::now()->format('Y-m-d')
             ]);
             if ($response->successful()) {
-                $campaignsReport = $response['data']['list'];
+                $campaignsReport = $response['data'];
                 foreach ($campaignsReport as $campaignReportData) {
                     $campaignReport = [];
-                    $campaignReport['conversion_rate'] = $campaignReportData['metrics']['conversion_rate'];
-                    $campaignReport['total_purchase_value'] = $campaignReportData['metrics']['total_purchase_value'];
-                    $campaignReport['offline_shopping_events_value'] = $campaignReportData['metrics']['offline_shopping_events_value'];
-                    $campaignReport['impressions'] = $campaignReportData['metrics']['impressions'];
-                    $campaignReport['clicks'] = $campaignReportData['metrics']['clicks'];
-                    $campaignReport['total_onsite_shopping_value'] = $campaignReportData['metrics']['total_onsite_shopping_value'];
-                    $campaignReport['spend'] = $campaignReportData['metrics']['spend'];
-                    $campaignReport['stat_time_day'] = $campaignReportData['dimensions']['stat_time_day'];
-                    $campaignReport['campaign_id'] = $campaignReportData['dimensions']['campaign_id'];
-                    CampaignsReport::create($campaignReport);
+                    $campaignReport['impressions'] = $campaignReportData['impressions'];
+                    $campaignReport['campaign_id'] = $campaignReportData['campaign_id'];
+                    $campaignReport['campaign_name'] = $campaignReportData['campaign_name'];
+                    $campaignReport['clicks'] = $campaignReportData['clicks'];
+                    $campaignReport['spend'] = $campaignReportData['spend'];
+                    $campaignReport['date_start'] = $campaignReportData['date_start'];
+                    $campaignReport['date_stop'] = $campaignReportData['date_stop'];
+                    $campaignReport['publisher_platform'] = $campaignReportData['publisher_platform'];
+                    $campaignReport['purchase_roas'] = json_encode($campaignReportData['purchase_roas']);
+
+                    if($campaignReport['platform'] == 'facebook')
+                    {
+                        FacebookCampaignReport::create($campaignReport);
+                    }else if($campaignReport['platform'] == 'instagram')
+                    {
+                        InstagramCampaignReport::create($campaignReport);
+                    }
+                    else if($campaignReport['platform'] == 'messenger')
+                    {
+                        MessengerCampaignReport::create($campaignReport);
+                    }
+                    else if($campaignReport['platform'] == 'audience_network')
+                    {
+                        AudienceNetworkCampaignReport::create($campaignReport);
+                    }
+
                 }
                 DB::commit();
                 return count($campaignsReport);
@@ -85,7 +107,70 @@ class metaData extends Command
             ], 500);
         }
     }
+    public function getAdsReport()
+    {
+        try {
+            DB::beginTransaction();
+            $apiEndpoint = 'https://graph.facebook.com/v18.0/act_825886441839608/insights';
+            $response = Http::withHeaders([
+//                'Access-Token' => config('services.tiktok.access_token'),
+            ])->get($apiEndpoint, [
+                'fields' => json_encode(["impressions","campaign_id","campaign_name","clicks","purchase_roas","spend","conversion_values","conversions"]),
+                'level' => 'ad',
+                'time_range' => json_encode(["since"=>Carbon::now()->format('Y-m-d'),
+                    "until"=>Carbon::now()->format('Y-m-d')]),
+                'time_increment' => '1',
+                'breakdowns' => 'publisher_platform',
+                'page_size' => 50,
+            ]);
+            if ($response->successful()) {
+                $adsReport = $response['data'];
+                foreach ($adsReport as $adReportData) {
+                    $adReport = [];
+                    $adReport['impressions'] = $adReportData['impressions'];
+                    $adReport['ad_id'] = $adReportData['ad_id'];
+                    $adReport['ad_name'] = $adReportData['ad_name'];
+                    $adReport['clicks'] = $adReportData['clicks'];
+                    $adReport['spend'] = $adReportData['spend'];
+                    $adReport['date_start'] = $adReportData['date_start'];
+                    $adReport['date_stop'] = $adReportData['date_stop'];
+                    $adReport['publisher_platform'] = $adReportData['publisher_platform'];
+                    $adReport['purchase_roas'] = json_encode($adReportData['purchase_roas']);
 
+                    if($adReport['platform'] == 'facebook')
+                    {
+                        FacebookAdReport::create($adReport);
+                    }else if($adReport['platform'] == 'instagram')
+                    {
+                        InstagramAdReport::create($adReport);
+                    }
+                    else if($adReport['platform'] == 'messenger')
+                    {
+                        MessengerAdReport::create($adReport);
+                    }
+                    else if($adReport['platform'] == 'audience_network')
+                    {
+                        AudienceNetwrokAdReport::create($adReport);
+                    }
+
+                }
+                DB::commit();
+                return count($adsReport);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to fetch TikTok ad information.',
+                ], $response->status());
+            }
+        } catch (\Exception $e) {
+            // An exception occurred, so we roll back the transaction and return an error response
+            DB::rollBack();
+            Log::info($e->getMessage());
+            return response()->json([
+                'message' => 'An error occurred while fetching TikTok ad information.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
 
 
 
---
+++ laravel/app/Models/AudienceNetwrokAdReport.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AudienceNetwrokAdReport extends Model
+{
+    protected $table = 'audience_network_ad_reports';
+
+}
