e252ad70953c5af56120a62542e69dc2cb08638c
Mahmoud Trro <mahdevtrro@two.sa>
2023-12-23 11:31:17 +0300

    access token

--- laravel/config/services.php
+++ laravel/config/services.php
@@ -30,7 +30,7 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-    'tiktok' => [
+    'tiktok2' => [
         'access_token' => env('TIKTOK_ACCESS_TOKEN'),
         'advertiser_id' => env('TIKTOK_ADVERTISER_ID')
 
