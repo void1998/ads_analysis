0ea3b7c410e34e851a0bd51485f5adc9450538fc
Mahmoud Trro <mahdevtrro@two.sa>
2023-12-23 11:15:53 +0300

    access token

--- laravel/config/services.php
+++ laravel/config/services.php
@@ -39,6 +39,9 @@ return [
         'refresh_token' => 'eyJraWQiOiJyZWZyZXNoLXRva2VuLWExMjhnY20uMCIsInR5cCI6IkpXVCIsImVuYyI6IkExMjhHQ00iLCJhbGciOiJkaXIifQ..uaCKF9wt5HIe1FjA.FAYcibKTQTC9p1Eml7Z567Iev9V18AyLjratdiE1eSkbQwNDakyi776aaOE4r0JN_IMzSksLTTYd0cagPDCHuauFZdOrZ4dlT2lZYgnHzRLNGjQ91URm4_Tr-9nZA4881pADo1up2_9iN8t8pQ_B2aoKaLhVDICfMKWCAsczoYDNQnYEMoXSN5NBLbUX57ut472liTscEn2MgyDVsu8Y2yjgVxHqfa4VvqN8TmM26Yv7hkdBaTPg-Zamu4kErfXpJ1tFMoKT4CTsCBo.95TNLnPu9-MxdiYPMZc0HA',
         'client_id' => 'c98e28be-3a15-44be-b0c5-7ff9445d482d',
         'client_secret' => '7d54b6641f3ad17b6c12'
+    ],
+    'meta' => [
+        'access_token' => 'EAAFLZAZC5BVxUBO7ZALlo4soEjatYAqjeiolsLBJJSYtmSqdBo0HNGPoA1Twax3O5FOYYlfX6ajkHxy3g2zjfiiN44voIEve4t6CheKUpKW4IeiXLEUrsHy6SKtwLdZAugei50ZA3Duso1BhMZCqsKJfc4C9S20CCM2NlOtupK9aisZAVPoID5MdzRm9MboHmOwszGg6rQUVGYf8FZB2'
     ]
 
 ];
